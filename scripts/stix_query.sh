26550c96121e03a8ecd78255a68c1c0caed149f3
Murad <mchowdh200@gmail.com>
2022-07-10 13:24:26 -0600

    missing pipe

--- scripts/stix_query.sh
+++ scripts/stix_query.sh
@@ -34,13 +34,13 @@ done
 # per sample hits for a given query region
 result=$(cd $index_path &&
       stix -s 500 -d $stix_db -i $stix_index -t $svtype -l $left -r $right |
-      tail -n+3 # chomp the two header lines
+      tail -n+3 | # chomp the two header lines
       awk 'BEGIN{OFS="\t"}{print $1,$7+$8}' # sample, paired+splits
       # awk '{s+=$1} END{print s}' # sum
    )
 
 # take the list of samples and make into a single line separated by tab
-samples=$(cut -f1 <<<$results | tr '\n' '\t')
-hits=$(cut -f2 <<<$results | tr '\n' '\t')
-[[ $print_header == true ]] && printf "\t\t\t\t$samples\n"
+samples=$(cut -f1 <<<$result | tr '\n' '\t')
+hits=$(cut -f2 <<<$result | tr '\n' '\t')
+[[ $print_header == "true" ]] && printf "\t\t\t\t$samples\n"
 printf "$left\t$right\t$hits\n"
